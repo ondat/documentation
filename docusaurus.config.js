883adfe956b836ac03acefb6856c6d094b8b4481
mvisintin <91465583+mvisintin@users.noreply.github.com>
2021-12-13 11:41:33 +0000

    Disable bot indexing

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,6 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
   organizationName: 'facebook', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
+  noIndex: true,
 
   presets: [
     [
